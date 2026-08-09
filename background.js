8cfad95bdd3bc84ed51a87a0b7fa3e9fd5cc6081
nauquu <nauquu@gmail.com>
2026-08-09 20:29:12 +0700

    feat: add Brave Browser Side Panel compatibility via background service worker

---
+++ background.js
@@ -0,0 +1,7 @@
+// Web Suite Pro - Background Service Worker for Brave & Chrome Side Panel Support
+
+chrome.runtime.onInstalled.addListener(() => {
+  if (chrome.sidePanel && chrome.sidePanel.setPanelBehavior) {
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: false }).catch(() => {});
+  }
+});
