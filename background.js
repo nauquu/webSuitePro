46160ec15c24ab80c49cd760c5385b6781be77a3
nauquu <nauquu@gmail.com>
2026-08-09 20:33:08 +0700

    fix: direct side panel toggle on extension action click for Brave Browser

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
 // Web Suite Pro - Background Service Worker for Brave & Chrome Side Panel Support
 
-chrome.runtime.onInstalled.addListener(() => {
-  if (chrome.sidePanel && chrome.sidePanel.setPanelBehavior) {
-    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: false }).catch(() => {});
+// Automatically configure click on Action icon to open Side Panel natively on Brave & Chrome
+if (typeof chrome !== 'undefined' && chrome.sidePanel) {
+  if (chrome.sidePanel.setPanelBehavior) {
+    chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(() => {});
   }
-});
+
+  if (chrome.action && chrome.action.onClicked) {
+    chrome.action.onClicked.addListener((tab) => {
+      if (tab && tab.windowId) {
+        chrome.sidePanel.open({ windowId: tab.windowId }).catch(() => {});
+      }
+    });
+  }
+}
